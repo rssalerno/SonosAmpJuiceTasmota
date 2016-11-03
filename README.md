c17b1bf6990a8d2e6ce9c270291e20e075c8c4f9
A <passporta@jodam.net>
2016-11-03 12:13:34 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-This is just a simple starting stub for automating on start/stop for a certain room. This takes grouping into account and act accordingly. 
+# SonosAmpJuicePi
 
-The most common usage scenario is controlling external AMPs (via network, relay or similar)
+## Purpose
+The purpose of this application is to turn power off and on at the mains for centrally-located amplifiers/receivers, using GPIO pins on the Raspberry Pi connected to relays (such as the PowerSwitch Tail).
 
-This was created by user request, and will not be maintained. It might get deleted at any time, so fork it if you want to keep it around.
+## App.js
+  - Edit the `amps` variable to associate each of your sonos player/room names with a GPIO physical pin number (which you in turn have connected to a relay that controls power to the corresponding amplifier).
+  - Edit the `stopTimeout` variable if you want to change the delay between when the application learns that an amp can be turned off and when it does so.
+  - Change the value of the `justTestingOnSomeDeviceThatLacksGpio` variable to `true` if you want to test the application on your PC (by running node and watching log files and/or command-line output).
+
+## Notes
+  - Responds appropriately to grouping, ungrouping, muting, pausing, stopping, playing, turning the volume all the way down to zero, and increasing the volume from zero
+  - Controls each player's amplifier as appropriate, whether or not it is currently part of a group
+  - Turns amplifiers on instantly when they are needed; turns them off after `stopTimeout` when they are not. `stopTimeout` will reset while users are making any additional changes such as pausing or starting and adjusting volume.
+  - Runs reliably and long-term on the author's Raspberry Pi 2
+  - Does not account for setups that include the Sonos Sub or Playbar, or that use stereo pairing (using one player for the left channel and another for the right)
+	- A player's amplifier may turn on upon being added to a group even when the group is not playing anything. It will turn off after `stopTimeout` if the group does not begin playing something in the meantime.
+
+## SonosAmpJuicePi.img.gz
+  - This is a ready-to-run image for Raspberry Pi 2, adapted from the one at https://github.com/jishi/node-sonos-http-api/releases/download/v1.0.1/sonos-http-api-1.0.1.img.gz.
+  - Files are exposed over the network via shares (Samba a.k.a SMB a.k.a. UNC), so you may not ever need to SSH into it (but note that root has no password and change that as appropriate for your setup!).
+  - Set up your room names etc. as specified above by editing `\\sonos\flash\apps\SonosAmpJuicePi\app.js`.
+  - See what is going on by opening the latest file in `\\sonos\flash\logs`.
+
+## Acknowledgement
+This is a trivial hack with a result that its author finds incredibly useful. It only exists because of the yeoman's work done by Jimmy Shimizu (https://github.com/jishi/). Thank you, Jimmy!!!
--- app.js
+++ app.js
@@ -1,50 +1,267 @@
 'use strict';
 
-var SonosDiscovery = require('sonos-discovery');
+const justTestingOnSomeDeviceThatLacksGpio = false;
+const stopTimeout = 60000; // in ms, delay between learning that an amp can be turned off and doing so
 
-var discovery = new SonosDiscovery();
-
-var coordinator;
-var roomName = 'Office';
-var stopTimer;
-var stopTimeout = 3000; // in ms, eq 30 seconds
-var isActive;
-
-// You can't get the player unless it has been discovered
-// On first topology-change, you will have found your system
-discovery.on('topology-change', function () {
-  // remove event handler if topology changes
-  if (coordinator)
-    coordinator.removeListener('transport-state', transportChangeHandler);
-
-  console.log('fetching player', roomName);
-  var player = discovery.getPlayer(roomName);
-  coordinator = player.coordinator;
-  coordinator.on('transport-state', transportChangeHandler);
+var amps = { 
+  "Living Room": { physicalPin: 31, stopTimer: null },
+  "Kitchen": { physicalPin: 33, stopTimer: null },
+  "Master Bedroom": { physicalPin: 35, stopTimer: null },
+  "Master Bathroom": { physicalPin: 37, stopTimer: null }
+}
+var logsPath = justTestingOnSomeDeviceThatLacksGpio ? './logs/' : '/flash/logs/';
+var winston = require('winston');
+// log levels:  error, warn, info, verbose, debug, silly
+// maxSize * maxFiles = total storage logs may use (in bytes)
+var logger = new (winston.Logger)({
+  transports: [
+	/*
+    new (winston.transports.Console)({ 
+      level: 'info', 
+      timestamp: true,
+			prettyPrint: true,
+			colorize: true
+    }),
+		*/
+    new (winston.transports.File)({ 
+      level: 'info', 
+      timestamp: true, 
+      maxsize: 1000000, 
+      maxFiles: 50,
+      filename: logsPath + 'AppLog.txt', 
+			prettyPrint: true,
+      json: false
+    })
+  ]
 });
+var discoveryModuleLogger = new (winston.Logger)({
+  transports: [
+	/*
+    new (winston.transports.Console)({
+      level: 'warning', 
+      timestamp: true,
+      prettyPrint: true,
+      colorize: true 
+    }),
+	*/
+    new (winston.transports.File)({ 
+      level: 'warn', 
+      timestamp: true, 
+      maxsize: 1000000, 
+      maxFiles: 50,
+      filename: logsPath + 'DiscoveryModuleLog.txt', 
+      json: false 
+    })
+  ]
+});
+console.log = function() {};
 
-function transportChangeHandler(transportData) {
-  var state = transportData.state;
-  if (!isActive && state.playerState == 'PLAYING') {
-    // Player is started, or is playing
-    console.log('Player is active');
-    isActive = true; // use this flag to know if the stop state is the first one after play.
-    clearTimeout(stopTimer);
+logger.info("Starting app.");
+ 
+var debounce = require('throttle-debounce/debounce');
 
-    // ADD code for starting playback here
+var SonosDiscovery = require('sonos-discovery');
+var discoverySettings = {
+    household: null,
+    log: discoveryModuleLogger
+  };
+var discovery = new SonosDiscovery(discoverySettings);
+logger.debug('START discovery');
+logger.debug(discovery);
+logger.debug('END discovery');
 
-    return;
-  }
+var rpio;
+if (!justTestingOnSomeDeviceThatLacksGpio) {
+  rpio = require('rpio'); // https://github.com/jperkin/node-rpio
+}
 
-  if (isActive && (state.playerState == 'STOPPED' || state.playerState == 'PAUSED_PLAYBACK')) {
-    clearTimeout(stopTimer);
-    isActive = false;
-    console.log('queueing a stop signal');
-    stopTimer = setTimeout(stopHandler, stopTimeout);
+var roomNames = Object.keys(amps);
+
+for (var i = 0; i != roomNames.length; i++) {
+	var roomName = roomNames[i];
+	var amp = amps[roomName];
+	logger.info(roomName + ': Ensuring amp is off at app startup (physical pin: ' + amp.physicalPin + ').');
+  if (!justTestingOnSomeDeviceThatLacksGpio) {
+    // Configure pin as an output pin, setting its initial state to low
+    rpio.open(amp.physicalPin, rpio.OUTPUT, rpio.LOW);
   }
 }
 
-function stopHandler() {
-  console.log('Player has been stopped for', stopTimeout, 'seconds');
-  // ADD code for stop here
+// Wait a quick beat in case of multiple events firing together, then fire on the last of them
+var juiceTheAmps = debounce(100, function (discovery) {
+	logger.debug('debounced function juiceTheAmps() called');
+	/*
+	
+	Example discovery.players with irrelevent properties removed and UUIDs replaced with English.
+
+	- Master Bathroom is operating independently:
+	
+	RINCON_MASTER_BATHROOM:
+		{ 
+			roomName: 'Master Bathroom',
+			uuid: 'RINCON_MASTER_BATHROOM',
+			state:
+			 { 
+				 volume: 69,
+				 mute: false,
+				 currentState: 'PLAYING'
+			 },
+			coordinator: { '$ref': '$["players"]["RINCON_MASTER_BATHROOM"]' },
+			groupState: { volume: 69, mute: false }
+		},
+
+	- Master Bedroom is coordinating Living Room:
+
+	RINCON_MASTER_BEDROOM:
+		{ 
+			roomName: 'Master Bedroom',
+			uuid: 'RINCON_MASTER_BEDROOM',
+			state:
+			{ 
+				volume: 39,
+				mute: false,
+				currentState: 'PAUSED_PLAYBACK'
+			},
+			coordinator: { uuid: 'RINCON_MASTER_BEDROOM' }
+			groupState: { volume: 51, mute: false } <-- groupState IS ONLY ACCURATE FOR coordinator
+		},
+	
+	RINCON_LIVING_ROOM:
+		{ 
+			roomName: 'Living Room',
+			uuid: 'RINCON_LIVING_ROOM',
+			state:
+			 { 
+				 volume: 63,
+				 mute: false,
+				 currentState: 'PLAYING' <-- THIS IS IRRELEVANT; SEE coordinator
+			 },
+			coordinator:
+			 { 
+				 roomName: 'Master Bedroom',
+				 uuid: 'RINCON_MASTER_BEDROOM',
+				 state:
+					{ 
+						volume: 39,
+						mute: false,
+						currentState: 'PAUSED_PLAYBACK'
+					},
+				 coordinator: { uuid: 'RINCON_MASTER_BEDROOM' }
+				 groupState: { volume: 51, mute: false } <-- groupState IS ONLY ACCURATE FOR coordinator
+			 }
+		}
+			 
+	*/
+	
+	/*
+	logger.debug('START discovery');
+	logger.debug(discovery);
+	logger.debug('END discovery');
+	logger.debug('START discovery.players');
+	logger.debug(discovery.players);
+	logger.debug('END discovery.players');
+	logger.debug(`discovery.players.length: ${discovery.players.length}`);
+	*/
+	
+	var uuids = Object.keys(discovery.players);
+	for(var i = 0, length = uuids.length; i < length; i++ ) {
+		var player = discovery.players[uuids[i]];;
+		if (!player) {
+			logger.warn(`discovery.players[${uuids[i]}]:`);
+			logger.warn(discovery.players[uuids[i]]);
+			continue;
+		}
+		var amp = amps[player.roomName];
+		if (!amp) {
+			logger.warn(`Discovered a player ("${player.roomName}") for which no amp is configured`);
+			continue;
+		}
+		logger.debug(`player.roomName: ${player.roomName}`);
+		logger.debug(`player.coordinator.state.currentState: ${player.coordinator.state.currentState}`);
+		logger.debug(`player.state.volume: ${player.state.volume}`);
+		logger.debug(`player.state.mute: ${player.state.mute}`);
+		logger.debug(`player.coordinator.groupState.volume: ${player.coordinator.groupState.volume}`);
+		logger.debug(`player.coordinator.groupState.mute: ${player.coordinator.groupState.mute}`);
+		var giveItJuice =
+			(player.coordinator.state.currentState == 'PLAYING') &&
+			player.state.volume && 
+			!player.state.mute &&
+			player.coordinator.groupState.volume &&
+			!player.coordinator.groupState.mute;
+		logger.debug(`giveItJuice: ${giveItJuice}`);
+		if (giveItJuice) {
+			logger.info(`${player.roomName}: player is playing and not muted; cancelling any stop timer`);
+			clearTimeout(amp.stopTimer);
+			logger.info(`${player.roomName}: turning/keeping amp on (physical pin: ${amp.physicalPin})`);
+			if (!justTestingOnSomeDeviceThatLacksGpio) {
+				rpio.write(amp.physicalPin, rpio.HIGH);
+			}
+		} else {
+			logger.info(`${player.roomName}: player is stopped (or muted); starting a timer to turn amp off`);
+			clearTimeout(amp.stopTimer);
+			var roomNameVal = player.roomName.valueOf();
+			amp.stopTimer = setTimeout(stopHandler, stopTimeout, roomNameVal);
+		}
+	}
+});
+
+logger.info("Setting up transport-state event");
+discovery.on('transport-state', transportStateChangeHandler);
+logger.info("Finished setting up transport-state event");
+
+logger.info("Setting up group-volume event");
+discovery.on('group-volume', groupVolumeChangeHandler);
+logger.info("Finished setting up group-volume event");
+
+logger.info("Setting up group-mute event");
+discovery.on('group-mute', groupMuteChangeHandler);
+logger.info("Finished setting up group-mute event");
+
+logger.info("Setting up volume event");
+discovery.on('volume', volumeChangeHandler);
+logger.info("Finished setting up volume event");
+
+logger.info("Setting up mute event");
+discovery.on('mute', muteChangeHandler);
+logger.info("Finished setting up mute event");
+
+function transportStateChangeHandler(data) {
+  logger.debug('transport-state event fired');
+  juiceTheAmps(discovery);
+}
+
+function groupVolumeChangeHandler(data) {
+  logger.debug('group-volume event fired');
+  // data.uuid,
+  // data.groupState,
+  // data.playerVolumes
+  juiceTheAmps(discovery);
+}
+
+function groupMuteChangeHandler(data) {
+  logger.debug('group-mute event fired');
+  // from sonos-discovery package:
+  // discovery.emit('group-mute', {uuid: _this.uuid, state: _this.groupState});
+  juiceTheAmps(discovery);
+}
+
+function volumeChangeHandler(data) {
+  logger.debug('volume event fired');
+  // from sonos-discovery package:
+  // discovery.emit('volume', {uuid: _this.uuid, state: _this.getState()});
+  juiceTheAmps(discovery);
+}
+
+function muteChangeHandler(data) {
+  logger.debug('mute event fired');
+  // from sonos-discovery package:
+  // discovery.emit('mute', {uuid: _this.uuid, state: _this.getState()});
+  juiceTheAmps(discovery);
+}
+
+function stopHandler(roomNameParam) {
+  logger.info(`${roomNameParam}: player stopped/muted at least ${stopTimeout / 1000} seconds; turning amp off (physical pin: ${amps[roomNameParam].physicalPin})`);
+  if (!justTestingOnSomeDeviceThatLacksGpio) {
+    rpio.write(amps[roomNameParam].physicalPin, rpio.LOW);
+  }
 }
